5c9fc6500d1931884a13e6e68424d724fbed6cc3
Shivam Kumar <shivamyuvraj4537@gmail.com>
2026-07-23 14:36:23 +0530

    Add Floyd-Warshall algorithm implementation
    Implement Floyd-Warshall algorithm for shortest paths and negative cycle detection.

---
+++ floyd_warshall.cpp
@@ -0,0 +1,25 @@
+class Solution {
+	public:
+	void floydWarshall(vector<vector<int>> &a) {
+		int n = a.size();
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					if (a[j][i] != 1e8 && a[i][k] != 1e8 && a[j][i] + a[i][k] < a[j][k]) {
+						
+						a[j][k] = a[j][i] + a[i][k];
+					}
+				}
+			}
+		}
+		
+		//detect negative cycle
+		int i=0;
+		while(i<n){
+		    if(a[i][i]<0){
+		        return -1;
+		    }
+		}
+	}
+};
